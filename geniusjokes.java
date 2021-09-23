20141cde4a84299d16e840f2175e7e2c61fc6994
JordanStein5 <85502279+JordanStein5@users.noreply.github.com>
2021-09-22 18:09:24 -0700

    Add files via upload
    Added Genius Jokes program

---
+++ geniusjokes.java
@@ -0,0 +1,10 @@
+package myProject;
+
+public class geniusjokes {
+	public static void main(String[] args) {
+	    System.out.println("Welcome, my name is Jordan");
+	    System.out.println("Jordan is not better than Lebron");
+	    System.out.println("Shaq is the best 3 point shooter ever");
+	    System.out.println("Curry isn't better than Kyrie");
+	  }
+}
